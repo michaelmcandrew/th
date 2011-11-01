f7c820fd85a4c95bd31214b8c84eb5af7159ec2f
KatyMoon <katyjockelson@thirdsectordesign.org>
2011-11-01 11:11:44 +0000

    removing $action_links

--- drupal/sites/all/themes/technically/templates/page--front.tpl.php
+++ drupal/sites/all/themes/technically/templates/page--front.tpl.php
@@ -122,11 +122,6 @@
 							</div>
 						<?php endif; ?>
 						<?php print render($page['help']); ?>
-						<?php if ($action_links): ?>
-							<ul class="action-links">
-								<?php print render($action_links); ?>
-							</ul>
-						<?php endif; ?>
 						<?php print render($page['content']); ?>
 					</div>
 				</div>
