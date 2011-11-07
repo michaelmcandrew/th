ec33df59923ba23f9a7a7fae38231227171ce846
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2011-11-07 19:01:02 +0000

    scanning now works

--- checkin_skel/bao.php
+++ checkin_skel/bao.php
@@ -3,21 +3,21 @@ require_once '/var/www/th/sites/default/civicrm.settings.php';
 require_once 'CRM/Core/Config.php';
 $config =& CRM_Core_Config::singleton( );
 
-define('TH_VISIT_ACTIVITY_TYPE_ID',35);
+define('TH_VISIT_ACTIVITY_TYPE_ID',34);
+define('TH_ACTIVITY_STATUS_IN_PROGRESS',7);
+define('TH_ACTIVITY_STATUS_COMPLETE',2);
 
-function start_visit($contact_id) {
-    
+function start_visit($contact_id, $location) {
+    echo "checking in...\n";
+	
 	//create activity: type=visit, status=in progress
 	$params = array( 
       'source_contact_id' => $contact_id,
       //checkin activity
       'activity_type_id' => TH_VISIT_ACTIVITY_TYPE_ID,
-      'subject' => 'London checkin  test',
+      'subject' => $location, // TODO: probably actually want to record this as a custom data field contact reference for Tech Hub organisations
       'activity_date_time' => date("Y-m-d H:i:s"),
-      //status 'in progress'
-      'status_id' => 7,
-      //'normal' priority
-      'priority_id' => 2,
+      'status_id' => TH_ACTIVITY_STATUS_IN_PROGRESS,
       'version' => 3,
       );
 
@@ -25,55 +25,37 @@ function start_visit($contact_id) {
 }
 
 function end_visit($contact_id) {
-	//foreach all open visits for this contact	
-	//update activity: type=visit, status=complete
+	echo "checking out...\n";
+	//get all open visits for this contact
 	$params = array( 
-      'contact_id' => $contact_id,
-      'activity_type_id' => TH_VISIT_ACTIVITY_TYPE_ID,
-      'version' => 3
-    );
+		'contact_id' => $contact_id,
+		'activity_type_id' => TH_VISIT_ACTIVITY_TYPE_ID,
+		'status_id' => TH_ACTIVITY_STATUS_IN_PROGRESS,
+		'version' => 3
+	);
 
-      $result = civicrm_api( 'activity','get',$params );
-      print_r($result);exit;
-      foreach ($result['values'] as $key => $activity){  // Bug in API.  Peter - please report and record issue number here
-          if($activity['activity_type_id']!=TH_VISIT_ACTIVITY_TYPE_ID){
-               unset($result['values'][$key]);
-           }
-       }
+    $result = civicrm_api( 'activity','get',$params );
+
+	// Bug in API.  Peter - please report and record issue number here
+	foreach ($result['values'] as $key => $activity){  
+		if($activity['activity_type_id']!=TH_VISIT_ACTIVITY_TYPE_ID OR $activity['status_id']!=TH_ACTIVITY_STATUS_IN_PROGRESS){
+			unset($result['values'][$key]);
+		}
+	}
        
       
-     // foreach ($result['values'] as $key => $activity){
-          
-          
-          //  $result=civicrm_api("Activity","update",$params);
-
-         }
-       
-       // after the above foreach loop, the results array looks like it should look
-       
-       //cycke thorugh all results
-       
-       //if the status is in prgogress close and update
-       
-       
-             // 
-             //           $params = array( 
-             //             'source_contact_id' => $contact_id,
-             //             //checkin activity
-             //             'activity_type_id' => 35,
-             //             'subject' => 'London checkin  test',
-             //             'activity_date_time' => date("Y-m-d H:i:s"),
-             //             //status 'completed'
-             //             'status_id' => 2,
-             //             //'normal' priority
-             //             'priority_id' => 2,
-             //             'version' => 3,
-             //             );
-             // 
-             //           $result=civicrm_api("Activity","create",$params);
-             // }
-          
-      
+	foreach ($result['values'] as $visit_params){
+		$visit_params['version']=3;
+		$visit_params['status_id']=TH_ACTIVITY_STATUS_COMPLETE;
+		
+		$start = new DateTime($visit_params['activity_date_time']);
+		$end = new DateTime();
+		$seconds = $end->getTimestamp()-$start->getTimestamp();
+		$visit_params['duration']=ceil($seconds/60);	
+		
+		
+		$result=civicrm_api("Activity","update",$visit_params);
+	}
 }
 
 
@@ -95,17 +77,25 @@ function is_in_building($contact_id){
 	$params = array( 
       'contact_id' => $contact_id,
       //visit activity
-      'activity_type_id' => 35,
+      'activity_type_id' => TH_VISIT_ACTIVITY_TYPE_ID,
       //status 'in progress'
-      'status_id' => 7,
+      'status_id' => TH_ACTIVITY_STATUS_IN_PROGRESS,
       'version' => 3,
       );
-	$result=civicrm_api("Activity","getcount", $params);
-    // echo $result;
-	if ($result > 0) {
-	    return True;
+
+  $result = civicrm_api( 'activity','get',$params );
+
+	// Bug in API.  Peter - please report and record issue number here
+	foreach ($result['values'] as $key => $activity){  
+		if($activity['activity_type_id']!=TH_VISIT_ACTIVITY_TYPE_ID OR $activity['status_id']!=TH_ACTIVITY_STATUS_IN_PROGRESS){
+			unset($result['values'][$key]);
+		}
 	}
-	    else {
-	        return False;
-	    }
+
+	$number_of_in_progress_visits=count($result['values']);
+	if ($number_of_in_progress_visits > 0) {
+	    return True;
+	}else {
+		return False;
+	}
 }
--- checkin_skel/scan.php
+++ checkin_skel/scan.php
@@ -2,13 +2,22 @@
 include 'bao.php';
 
 
-function scan($contact_id) {
+function scan($contact_id, $location) {
 	
 	if(is_in_building($contact_id)){
 		end_visit($contact_id);
 	} else
-		start_visit($contact_id);
+		start_visit($contact_id, $location);
 }
 
 
-scan(1710);  
+scan(1710, 'London');  
+
+
+// $start='2011-11-07 18:38:50';
+// $end=;
+
+// 
+// 
+// 
+// echo "$start\n$end\n";
